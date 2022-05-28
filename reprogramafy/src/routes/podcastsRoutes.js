545e8143b6a1d1880e95ab641961207611409281
Carol Alves <prof.carolalves90@gmail.com>
2022-05-28 17:36:40 -0300

    :rocket: passando rotas

--- reprogramafy/src/routes/podcastsRoutes.js
+++ reprogramafy/src/routes/podcastsRoutes.js
@@ -4,5 +4,9 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/biblioteca', controller.getAllPods)
+router.get('/biblioteca/tema', controller.getTopics)
+router.post("/adicionar", controller.addPods)
+router.patch("/atualizar/:id", controller.atualizarPods)
+router.delete("/delete/:id", controller.deletePods)
 
 module.exports = router
