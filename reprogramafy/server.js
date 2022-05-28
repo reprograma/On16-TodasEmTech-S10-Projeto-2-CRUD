d0f770466d8a228981e49c9bb2b6e47c97f5473f
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-05-28 11:00:54 -0300

    Criação do nosso server

---
+++ reprogramafy/server.js
@@ -0,0 +1,11 @@
+// Configurando a porta do nosso servidor - Criando nosso server
+
+const app = require('./src/app') // Chamando o arquivo app.js
+
+const PORT = 1313 // Configuro a porta
+
+
+// Iniciando server
+app.listen(PORT, () => {
+    console.log(`Seu servidor está na porta ${PORT}`)
+})
