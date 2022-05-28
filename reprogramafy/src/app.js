e65d729eff51482b02d050b91af39eb508009be4
clarasavelli <draclarasavelli@gmail.com>
2022-05-28 14:43:26 -0300

    nova rota

--- reprogramafy/src/app.js
+++ reprogramafy/src/app.js
@@ -4,7 +4,7 @@ const app = express() // executo o express
 
 app.use(express.json()) // use o body parser
 
-const podRotas = require('./routes/podcastRoutes') // importe da continuação de rotas podcasts
+const podRotas = require('./routes/podcastsRoutes') // importe da continuação de rotas podcasts
 
 app.use("/podcast", podRotas) // digo para meu app qual rota é minha rota raiz
 
--- reprogramafy/src/controller/podcastsController.js
+++ reprogramafy/src/controller/podcastsController.js
@@ -1,1 +1,33 @@
-const podcasts = reequire
+// importar meu JSON de podcasts
+const podcasts = require('../models/podcasts.json')
+
+// retorna todos os podcasts
+
+const getAllPods = (request, response) => {
+    try {
+        response.status(200).json([{
+            "Podcasts": podcasts
+        }])
+    } catch (err) {
+        response.status(500).send({ message: "Erro no server" })
+    }
+
+}
+
+const getTopics = (request, response) => {
+    const topicRequest = request.query.topic
+    const topicFilter = podcasts.filter(podcasts => podcasts.topic.includes(topicRequest))
+    if (topicFilter.length > 0) {
+        response.status(200).send(topicFilter)
+    } else {
+        response.status(404).send([{
+            Message: "não encontrado"}])
+    }
+}
+
+
+
+module.exports = {
+    getAllPods,
+    getTopics,
+}
