a86be1a2d83ff1a1ebc35b975928abff9181d9ab
Gaia Maria <gaiamaria.melos@gmail.com>
2022-05-30 18:20:24 -0300

    Finalização das rotas e controllers - testadas

---
+++ reprogramafy/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express'); //importo o express
+
+const app = express()// executo o express
+
+app.use(express.json()) // uso o body parse
+
+// importe da continuação de rotas podcast
+const podRotas = require('./routes/podcastsRoutes')
+
+app.use('/podcast', podRotas)// crio uma rota raiz
+
+const musicRoutes = require('./routes/musicasRoutes')
+
+app.use('/reprogramafy', musicRoutes)
+// exportando para usar o server.js
+module.exports = app
---
+++ reprogramafy/src/routes/musicasRoutes.js
@@ -0,0 +1,19 @@
+const controller = require("../controller/musicasController");
+
+const express = require("express");
+
+// funcao de rotas de express
+const router = express.Router();
+
+// router. metodo http (rota, funcao)
+
+router.get("/biblioteca", controller.getAllMusic);
+router.get("/biblioteca/musicas/:id", controller.getMusic)
+router.get("/biblioteca/artistas", controller.getArtist)
+router.post("/adicionar", controller.addMusic)
+router.put("/alterar/:id", controller.updateMusic)
+router.delete("/deletar/:id", controller.deleteMusic)
+router.patch("/atualizar/:id", controller.updateFav)
+
+
+module.exports = router;
