85bf916667e038c6407818b22e24757c5c98b1a2
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-05-31 14:04:44 -0300

    Criação das rotas

--- reprogramafy/src/routes/podcastsRoutes.js
+++ reprogramafy/src/routes/podcastsRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router()
 router.get("/biblioteca", controller.getAllPods)
 router.get("/biblioteca/tema", controller.getTopics)
 router.post("/adicionar", controller.addPods)
-//router.delete("/delete/:id", controller.deletePods)
+router.delete("/delete/:id", controller.deletePods)
 router.patch("/atualizar/:id", controller.atualizarPods)
 
 
