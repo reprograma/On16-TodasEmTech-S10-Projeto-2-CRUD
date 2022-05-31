9a085f95d1a55f83d42f03a392440ed5c009ec3d
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-05-31 17:08:23 -0300

    Criando nossas rotas

---
+++ reprogramafy/src/routes/musicasRoutes.js
@@ -0,0 +1,20 @@
+// Criando nossas rotas
+
+const controller = require('../controller/musicasController') // chama o controller de podcasts
+
+const express = require('express') // chamando o express
+
+// Função de rotas do express
+const router = express.Router()
+
+// router, metodo http (rota, função)
+router.get("/musics", controller.getAllMusics)
+router.get("/musics/:id", controller.getMusicId)
+router.get("/musics/artists", controller.getMusicArtist)
+router.post("/musics", controller.addMusic)
+router.put("/musics/:id", controller.updateMusic)
+router.delete("/musics/:id", controller.deleteMusic)
+router.patch("/musics/:id/favorited", controller.favoritedMusic)
+
+// Exportar para ser usado no app.js
+module.exports = router
