03d3cd54f34e2848916b905dbc01b9d7c6f6f4e6
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-05-31 13:37:39 -0300

    Criando nossas rotas

--- reprogramafy/src/routes/podcastsRoutes.js
+++ reprogramafy/src/routes/podcastsRoutes.js
@@ -9,8 +9,10 @@ const router = express.Router()
 
 // router, metodo http (rota, função)
 router.get("/biblioteca", controller.getAllPods)
-
-
+router.get("/biblioteca/tema", controller.getTopics)
+router.post("/adicionar", controller.addPods)
+//router.delete("/delete/:id", controller.deletePods)
+router.patch("/atualizar/:id", controller.atualizarPods)
 
 
 // Exportar para ser usado no app.js
