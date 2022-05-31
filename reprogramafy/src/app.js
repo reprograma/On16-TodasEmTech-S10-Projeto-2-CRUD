97d2cd26a4dcd021f662dbb74bb5dd7a227fbf16
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-05-31 17:06:49 -0300

    Centralizando o conteudo da aplicação

--- reprogramafy/src/app.js
+++ reprogramafy/src/app.js
@@ -10,5 +10,8 @@ app.use(express.json()) // Fazendo o body parser
 const podRotas = require('./routes/podcastsRoutes') // Importe da continuação de rotas podcasts
 app.use("/podcast", podRotas) // Criando rota raiz "/podcast"
 
+const musicRoutes = require('./routes/musicasRoutes')
+app.use('/reprogramafy', musicRoutes) 
+
 
 module.exports = app // Exportando para usar o server.js
