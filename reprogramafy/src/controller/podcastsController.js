e62a438bccc0b5acc1acba6379247ede8fbfe65d
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-05-31 13:36:41 -0300

    Criando a lógica das rotas

--- reprogramafy/src/controller/podcastsController.js
+++ reprogramafy/src/controller/podcastsController.js
@@ -3,6 +3,9 @@
 const {
     response
 } = require('express')
+const {
+    request
+} = require('../app')
 const podcasts = require('../models/podcasts.json')
 
 // Retorna todos os podcasts
@@ -18,6 +21,66 @@ const getAllPods = (request, response) => {
     }
 }
 
+const getTopics = (request, response) => {
+    const topicRequest = request.query.topic
+    const topicFilter = podcasts.filter(podcasts => podcasts.topic.includes(topicRequest))
+    if (topicFilter.length > 0) {
+        response.status(200).send(topicFilter)
+    } else {
+        response.status(404).send([{
+            "message": "Tópico não encontrado"
+        }])
+    }
+}
+
+const addPods = (request, response) => {
+    try {
+        let nameRequest = request.body.name
+        let podcasterRequest = request.body.podcaster
+        let topicRequest = request.body.topic
+        let starsRequest = request.body.stars
+
+        let newPodcast = {
+            id: Math.floor(Date.now() * Math.random()).toString(36), // gera ID randomico
+            name: nameRequest,
+            podcaster: podcasterRequest,
+            topic: topicRequest,
+            stars: starsRequest
+        }
+        podcasts.push(newPodcast)
+        response.status(201).json([{
+            "message": "Podcast novo cadastrado",
+            newPodcast
+        }])
+    } catch (err) {
+        console.log(err)
+        response.status(500).send([{
+            "message": "Erro interno ao cadastrar"
+        }])
+    }
+}
+
+const atualizarPods = (request, response) => {
+    const idRequest = request.params.id
+    const starsRequest = request.body.stars
+    starsFilter = podcasts.find((podcasts) => podcasts.id == idRequest)
+
+    if (starsFilter) {
+        starsFilter.stars = starsRequest
+        response.status(200).json([{
+            message: "Classificação atualizada com sucesso",
+            podcasts
+        }])
+    } else {
+        response.status(404).json([{
+            message: "Não foi modificado, gata"
+        }])
+    }
+}
+
+
+
+
 
 
 
@@ -25,5 +88,8 @@ const getAllPods = (request, response) => {
 
 
 module.exports = {
-    getAllPods
+    getAllPods,
+    getTopics,
+    addPods,
+    atualizarPods
 }
