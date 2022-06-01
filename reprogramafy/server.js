b06332bf6fb612fb33e172cf11c4420d1f309172
Sara <saragiannini593@gmail.com>
2022-06-01 18:40:29 -0300

    configura server

--- reprogramafy/server.js
+++ reprogramafy/server.js
@@ -1,8 +1,6 @@
-const app = require("./src/app") //chamando o arquivo app
+const app = require('./src/app')
+const PORT = 3030 
 
-const PORT = 1313 //PORTA
-
-//iniciando o servidor
-app.listen(PORT , ()=>{
-    console.log(`eaiiii, to na porta ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Servidor na porta ${PORT}`)
 })
