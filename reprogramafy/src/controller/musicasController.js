9fa1ce932e52c877bdda3a1fa64214686d71a6ff
caroliinaasilva <carolinadesousa84@yahoo.com>
2022-06-02 12:57:36 -0300

    add

--- reprogramafy/src/controller/musicasController.js
+++ reprogramafy/src/controller/musicasController.js
@@ -1,21 +1,34 @@
-const music = require ('../models/musicas.json')
-
+const music = require('../models/musicas.json')
 
 const getAllMusic = (request, response) => {
     try {
-      response.status(200).json([
-        {
-          Musicas: music,
-        },
-      ]);
+        response.status(200).json([
+            {
+                Musicas: music,
+            },
+        ]);
     } catch (err) {
-      response.status(500).send({ message: "Erro no server" });
+        response.status(500).send({ message: "Erro no server" });
     }
-  };
+};
+const getMusic = (request, response) => {
+    const especifMusic = request.query.title
+    const musicFilter = music.filter((musica) => musica.title.includes(especifMusic))
+    if (musicFilter.length > 0) {
+        response.status(200).send(musicFilter);
+    } else {
+        response.status(404).send([
+            {
+                message: "Musica não Encontrada",
+            },
+        ]);
+    }
+};
 
 
-  module.exports = {
-    getAllMusic
-    
+module.exports = {
+    getAllMusic,
+    getMusic
+
 }
 
--- reprogramafy/src/routes/musicasRoutes.js
+++ reprogramafy/src/routes/musicasRoutes.js
@@ -3,6 +3,10 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/playlist", controller.getAllMusic)
-
-
+router.get("/musica", controller.getMusic)
+// router.get("/musica/artista", contoller.getArtist)
+// router.post("/cadastar", controller.cadastro)
+// router.put('/atulizar/:id', controler.update)
+// router.delete('/delete', controller.deleteMusic)
+// router.patch('/favorita/:id', controller.favorited)
 module.exports = router
