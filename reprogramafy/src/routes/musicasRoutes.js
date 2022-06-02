a24c736e816dfa208dee03111d132480236640bc
Carol Alves <prof.carolalves90@gmail.com>
2022-06-01 21:59:52 -0300

    :sparkles: rota de cadastro

--- reprogramafy/src/routes/musicasRoutes.js
+++ reprogramafy/src/routes/musicasRoutes.js
@@ -1,9 +1,15 @@
 const controller = require('../controller/musicasController')
 const express = require('express')
+const { Router } = require('express')
 
 const router = express.Router()
 
 router.get('/biblioteca', controller.getAllSongs)
+router.get('/artista', controller.getArtista)
 router.get('/buscarmusica/:id', controller.getMusic)
+router.post('/cadastrar', controller.addSong)
+//router.put('/atualizar', controller.updateSong)
+//router.delete('deletar/:id', controller.deleteSong)
+//router.patch('favorita/:id', controller.updatefavorited)
 
 module.exports = router
